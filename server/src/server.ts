120c4ae8cdca558bf377fe43f7c96253f92b6747
Maurice Zuniga <mauricek12d@gmail.com>
2025-02-21 12:01:31 -0500

    Fixed the pathway to the client dist file in server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -60,10 +60,10 @@ async function startApolloServer() {
 
   // If in production, serve static assets
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../../client/build')));
+    app.use(express.static(path.join(__dirname, '../../client/dist')));
 
     app.get('*', (_, res) => {
-      res.sendFile(path.join(__dirname, '../../client/build/index.html'));
+      res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
     });
   }
 
